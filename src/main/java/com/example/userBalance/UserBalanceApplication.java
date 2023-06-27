6276dfd5f59c23348fb4dcefdb005146a40f12f1
kolosova_aa <kolosova_aa@interrao.ru>
2023-06-27 22:37:06 +0300

    initial commit

---
+++ src/main/java/com/example/userBalance/UserBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.userBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userBalance/UserBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userBalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
